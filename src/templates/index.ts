d100bfce4d066ce3c8b3efaa732dad2afb61e3db
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-06-21 20:56:26 +0530

    started with a fresh new project

--- src/templates/index.ts
+++
@@ -1,4 +0,0 @@
-export { packageJsonName, packageJson, packageJsonString } from "./packageJson";
-export { tsConfig } from "./tsConfig";
-export { readMe } from "./readme";
-export { App, index } from "./components";
