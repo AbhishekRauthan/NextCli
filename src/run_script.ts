43a48568d3684c159bd4e049ff2d6241afdb130c
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-06-22 14:56:21 +0530

    added a function to  install npm packages

---
+++ src/run_script.ts
@@ -0,0 +1,18 @@
+import pro = require("child_process");
+
+
+const installPackage = (cmd: string) => {
+  const child = pro.exec(cmd, function (error, stdout, stderr) {
+    if (stderr !== null) {
+      console.log("" + stderr);
+    }
+    if (stdout !== null) {
+      console.log("" + stdout);
+    }
+    if (error !== null) {
+      console.log("" + error);
+    }
+  });
+};
+
+module.exports = installPackage
